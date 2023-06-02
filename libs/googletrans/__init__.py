92d7d49adcba6df4852afa755c42fde23af11560
Yinan <95043151+yinan-c@users.noreply.github.com>
2023-06-02 23:16:11 +0100

    add py-googletrans

---
+++ libs/googletrans/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '3.1.0-alpha'
+
+
+from googletrans.client import Translator
+from googletrans.constants import LANGCODES, LANGUAGES  # noqa
--- translate.py
+++ translate.py
@@ -1,11 +1,7 @@
 import os
-import openai
-import deepl
 import json
 import sys
-import translators as ts
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 auth_key = os.getenv("DEEPL_API_KEY")
 
 deepl_check = os.getenv("deepl_check")
@@ -16,6 +12,7 @@ baidu_check = os.getenv("baidu_check")
 youdao_check = os.getenv("youdao_check")
 
 target_language = os.getenv("target_language")
+
 if target_language.lower() in ['en-gb', 'en-us']:
     translators_language = 'en'
 else:
@@ -34,10 +31,12 @@ json_output = {
 
 # OpenAI Translation
 if openai_check == '1' and sys.argv[1] == 'openai':
+    import openai
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
         messages = [
-            {"role": "system", "content" : "You are a translation expert proficient in various languages that can only translate text into {target_language}. Translate any thing I say from now on, do not engage a conversion with me, you can only return the translated text."},
+            {"role": "system", "content" : f"You are a translation expert proficient in various languages that can only translate text into {target_language}. Translate any thing I say from now on, do not engage a conversion with me, you can only return the translated text."},
             {"role": "user", "content" : f"{input_text}"}
         ])
     #prompt_text = f"You are a translation expert proficient in various languages that can only translate text and cannot interpret it. You can only return the translated text. Please translate the following text to {target_language}: {input_text}"
@@ -65,6 +64,7 @@ if openai_check == '1' and sys.argv[1] == 'openai':
 
 # DeepL Translation
 if deepl_check == '1' and sys.argv[1] == 'deepl':
+    import deepl
     deepl_translator = deepl.Translator(auth_key) 
     deepl_result = deepl_translator.translate_text([input_text], target_lang=target_language) 
     deepl_text = deepl_result[0].text
@@ -80,8 +80,9 @@ if deepl_check == '1' and sys.argv[1] == 'deepl':
     }
     json_output["items"].append(deepl_output)
 
-def translation_output(check, translator_name, icon_path, ts, input_text, target_language, json_output):
+def translation_output(check, translator_name, icon_path, input_text, target_language, json_output):
     if check == '1':
+        import translators as ts
         translated_text = ts.translate_text(input_text, to_language=target_language, translator=translator_name)
         output = {
             "type": "default",
@@ -95,15 +96,28 @@ def translation_output(check, translator_name, icon_path, ts, input_text, target
         json_output["items"].append(output)
     return json_output
 
+## Google Translation
+if google_check == '1' and sys.argv[1] == 'google':
+    from googletrans import Translator
+    translator = Translator()
+    translated_text = translator.translate(input_text, dest=target_language).text
+    google_output = {
+        "type": "default",
+        "subtitle": "Google Translate",
+        "title": translated_text,
+        "arg": translated_text,
+        'icon': {
+            'path': "./assets/google.png"
+        }
+    }
+    json_output["items"].append(google_output)
+
 if sys.argv[1] == 'bing':
-    json_output = translation_output(bing_check, 'bing', "./assets/bing.png", ts, input_text, translators_language, json_output)
-if sys.argv[1] == 'google':
-    json_output = translation_output(google_check, 'google', "./assets/google.png", ts, input_text, translators_language, json_output)
+    json_output = translation_output(bing_check, 'bing', "./assets/bing.png", input_text, translators_language, json_output)
 if sys.argv[1] == 'baidu':
-    json_output = translation_output(baidu_check, 'baidu', "./assets/baidu.png", ts, input_text, translators_language, json_output)
+    json_output = translation_output(baidu_check, 'baidu', "./assets/baidu.png",  input_text, translators_language, json_output)
 if sys.argv[1] == 'youdao':
-    json_output = translation_output(youdao_check, 'youdao', "./assets/youdao.png", ts, input_text, translators_language, json_output)
-        
+    json_output = translation_output(youdao_check, 'youdao', "./assets/youdao.png", input_text, translators_language, json_output)
 # Check if no translation method selected
 if deepl_check == '0' and openai_check == '0' and bing_check == '0' and google_check == '0' and baidu_check == '0' and youdao_check == '0':
     no_selection_output = {
